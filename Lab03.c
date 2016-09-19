d241e6fb44ab998360032605aa7db57722ebf00b
ITE03050132 <bblp123456789@gmail.com>
2016-09-19 05:04:32 -0400

    round 2

--- Lab03.c
+++ Lab03.c
@@ -4,8 +4,9 @@
 int main(void)
 {
 	int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10;
-	printf("Please key the ten numbers,use space between them :")
+	printf("Please key the ten numbers,use space between them :");
 	scanf("%d %d %d %d %d %d %d %d %d %d",&n1,&n2,&n3,&n4,&n5,&n6,&n7,&n8,&n9,&n10);
-	printf("Your numbers :%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",n1,n2,n3,n4,n5,n6,n7,n8,n9,n10);
+	printf("Your numbers :\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",n1,n2,n3,n4,n5,n6,n7,n8,n9,n10);
+	system("pause");
 	return 0;
 }
