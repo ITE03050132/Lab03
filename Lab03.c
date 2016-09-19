f26eb47d721750f1c05944108829b79493494a3b
ITE03050132 <bblp123456789@gmail.com>
2016-09-19 04:57:28 -0400

    date

---
+++ Lab03.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10;
+	printf("Please key the ten numbers,use space between them :")
+	scanf("%d %d %d %d %d %d %d %d %d %d",&n1,&n2,&n3,&n4,&n5,&n6,&n7,&n8,&n9,&n10);
+	printf("Your numbers :%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n",n1,n2,n3,n4,n5,n6,n7,n8,n9,n10);
+	return 0;
+}
